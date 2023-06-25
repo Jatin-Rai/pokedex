ba34379b9e4e3334a8964e46c8bf90b298909d80
Jatin-Rai <jatinrai258@gmail.com>
2023-06-25 15:26:40 +0530

    added dynamic year change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,17 +4,20 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 
 const Footer = () => {
-    const currentYear = new Date().getFullYear();
+  const currentYear = new Date().getFullYear();
   return (
     <footer className="bg-dark pt-4 text-center text-white">
       <Container>
         <Row>
           <Col md={6}>
-            <p>Copyright&copy; {currentYear} by Jatin Rai | All Rights Reserved.</p>
+            <p>
+              Copyright&copy; {currentYear} by Jatin Rai | All Rights Reserved.
+            </p>
           </Col>
-          <Col md={6}>
-            <p></p>
-          </Col>
+          {/* <Col md={6}>
+            <p>
+            </p>
+          </Col> */}
         </Row>
       </Container>
     </footer>
